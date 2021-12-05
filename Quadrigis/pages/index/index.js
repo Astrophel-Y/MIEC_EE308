7c4cb6f11ba394ddb421f4487e382142b661a38b
Astrophel-Y <Astrophely@outlook.com>
2021-12-05 21:52:55 +0800

    0.0.0.6a

--- Quadrigis/pages/index/index.js
+++ Quadrigis/pages/index/index.js
@@ -22,12 +22,15 @@ Page({
       url: '../index/repository/index'
     })
   },
+
   getComponent: function () {
     const comp = this.selectComponent('.pulldown');
   },
-  editpin() {
+
+  editPin() {
 
   },
+
   createRepo() {
 
   },
@@ -40,6 +43,7 @@ Page({
     }
     this.getComponent.selectData=['1','2'];
   },
+  
   getUserProfile(e) {
     // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认，开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
     wx.getUserProfile({
@@ -52,5 +56,54 @@ Page({
         })
       }
     })
-  }
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload: function () {
+
+    },
+
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh: function () {
+
+    },
+
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom: function () {
+
+    },
+
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage: function () {
+
+    }
 })
